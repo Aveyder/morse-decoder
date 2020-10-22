91e31eb685e7a7fa58fe4d28fc345f82825c12bf
Vitali_Vianhura <vengura.code@gmail.com>
2020-10-22 23:14:30 +0300

    feat: implement decode function

--- src/index.js
+++ src/index.js
@@ -38,7 +38,27 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const resultArray = [];
+    for (let i = 0; i < expr.length; i += 10) {
+        const part = expr.substring(i, i + 10);
+        if (part === "**********") {
+            resultArray.push(" ");
+        } else {
+            const morseArray = [];
+            const normalized = String(Number(part));
+            for (let j = 0; j < normalized.length; j += 2) {
+                const char = normalized.substring(j, j + 2);
+                if (char === "10") {
+                    morseArray.push(".");
+                } else {
+                    morseArray.push("-");
+                }
+            }
+            const morse = morseArray.join("");
+            resultArray.push(MORSE_TABLE[morse])
+        }
+    }
+    return resultArray.join("");
 }
 
 module.exports = {
